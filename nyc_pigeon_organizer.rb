30d2e2513e51d3d9ec54ea74a00bfa6f2ef19559
Rafael Salvador Valdez <manrvaldez.92@gmail.com>
2020-03-02 03:13:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,3 +6,10 @@ def nyc_pigeon_organizer(data)
   data.each do |attribute, items|
     items.each do |feature, array|
       array.each do |name|
+        names << name if !names.include?(name)
+      end 
+    end 
+  end 
+  
+names.each do |nickn|
+  pigeon_hash
